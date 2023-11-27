340b31bcb8c55b87bf04301f61278d4352abaad0
Luma Souza <125762406+lumasouza@users.noreply.github.com>
2023-11-26 21:31:42 -0300

    Update install-ec2.sh

--- install-ec2.sh
+++ install-ec2.sh
@@ -4,8 +4,8 @@ echo -e '\e[1;36m Boas vindas à instalação do PayAttention! para Linux \e[m'
 echo -e '\e[1;36m Vamos iniciar as primeiras configurações \e[m'
 
 echo -e '\e[1;35m VERIFICANDO PACOTES DO SISTEMA \e[m'
-sudo apt update && sudo apt upgrade
-sudo apt install curl
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl -y
 
 echo " "
 echo -e '\e[1;35m INSTALAR JAVA \e[m'
@@ -31,21 +31,15 @@ if command -v docker &>/dev/null;
 		sudo systemctl start docker
 		sudo systemctl enable docker
 		sudo curl -LO https://raw.githubusercontent.com/MindBridge-INC/jar-ec2/main/script_create_mysql_docker.sql
-		#sudo curl -LO https://raw.githubusercontent.com/MindBridge-INC/jar-ec2/main/Dockerfile
-		#sudo docker build -t mysql_mb
-		#sudo docker pull mysql:5.7
 		sudo docker run -d -p 3306:3306 --name ContainerMysql -e "MYSQL_DATABASE=Mindbridge_maquina" -e "MYSQL_ROOT_PASSWORD=buzzhenge" mysql:5.7
 		sudo docker exec -i ContainerMysql mysql -uroot -pbuzzhenge Mindbridge_maquina < script_create_mysql_docker.sql
 	else
 		echo -e '\e[1;33m Parece que o Docker não está instalado em seu dispositivo. Instalando... \e[m'
-		sudo apt install docker.io
+		sudo apt install docker.io -y
 		echo -e  '\e[1;36m Configurando Docker..... \e[m'
 		sudo systemctl start docker
 		sudo systemctl enable docker
 		sudo curl -LO https://raw.githubusercontent.com/MindBridge-INC/jar-ec2/main/script_create_mysql_docker.sql
-		#sudo curl -LO https://raw.githubusercontent.com/MindBridge-INC/jar-ec2/main/Dockerfile
-		#sudo docker build -t mysql_mb
-		#sudo docker pull mysql:5.7
 		sudo docker run -d -p 3306:3306 --name ContainerMysql -e "MYSQL_DATABASE=Mindbridge_maquina" -e "MYSQL_ROOT_PASSWORD=buzzhenge" mysql:5.7
 		sudo docker exec -i ContainerMysql mysql -uroot -pbuzzhenge Mindbridge_maquina < script_create_mysql_docker.sql
 fi
@@ -53,9 +47,9 @@ fi
 echo " "
 echo -e '\e[1;35m INSTALAR PAYATTENTION \e[m'
 
-echo -e '\e[1;33m Instalando... (s/n) \e[m'
+echo -e '\e[1;33m Instalando... \e[m'
 
 echo -e '\e[1;33m Baixando PayAttention... \e[m'
 sudo curl -LO https://github.com/MindBridge-INC/jar-ec2/raw/main/payAttention-1.0-SNAPSHOT-jar-with-dependencies.jar
 echo -e '\e[1;36m Instalação finalizada! \e[m'
-echo -e '\e[1;36m Para seguir com a execução do arquivo, digite java -jar payAttention-1.0-SNAPSHOT.jar \e[m'
+echo -e '\e[1;36m Para seguir com a execução do arquivo, digite java -jar payAttention-1.0-SNAPSHOT-jar-with-dependencies.jar \e[m'
